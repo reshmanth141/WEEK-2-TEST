e0e4ab02d7eb70b52f4d54812085b8d6cd50355f
reshmanth141 <reshmanth141@gmail.com>
2026-09-21 16:22:56 -0400

    Add files via upload

---
+++ python_logic_function.py
@@ -0,0 +1,7 @@
+def check_names(names):
+    for name in names:
+        print(name.upper(), "-", len(name), "letters")
+names = ["resmanTh", "Pavan", "David", "ChaiTaNya"]
+check_names(names)
+
+    
